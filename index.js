b45c49f9d433f87231fbf810ed5cd649d72f97d5
Your Name <you@example.com>
2022-05-24 16:41:37 -0300

    agregando tabla dinamica y chat

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config(); 
 const express = require('express'); 
 const {Server:ioServer} = require('socket.io'); 
 const morgan = require('morgan'); 
@@ -5,25 +6,44 @@ const http = require('http');
 const app = express(); 
 const httpServer = http.createServer(app); 
 const io = new ioServer(httpServer);
-const router = require('./routers/routes'); 
+const products = []; 
+const messages = []; 
+
 
 app.use(morgan('dev')); 
 app.use(express.json()); 
 app.use(express.urlencoded({extended:true})); 
 app.use(express.static(__dirname +'/public')); 
-app.use('/',router)
+
 
 app.set('views', 'public/views') 
 app.set('view engine','ejs'); 
 
+app.get('/',(req,res) =>{
+    res.render('index.ejs',{products})
+})
+
 
 io.on('connection',(client) =>{
     console.log('websocket funcionando',client.id);
-    
+    client.emit('productos',products)
+
+    client.on('newProduct',(product) =>{
+        products.push(product); 
+        io.sockets.emit('productos',products); 
+        console.log(products);
+    })
+
+    client.emit('messages',messages); 
+    client.on('newMessage',(message) =>{
+        messages.push(message); 
+        io.sockets.emit('messages',messages)
+    })
 })
 
-const PORT = 8080; 
+const PORT = process.env.PORT || 3030; 
 
 httpServer.listen(PORT,() =>{
     console.log(`Server on port ${httpServer.address().port}`);
 }); 
+
--- public/main.js
+++ public/main.js
@@ -1,4 +1,83 @@
 const socket = io(); 
+const form = document.getElementById('form'); 
+const nombre = document.getElementById('nombre'); 
+const precio = document.getElementById('precio'); 
+const url = document.getElementById('url'); 
+const productos = document.getElementById('productos')
+const enviar = document.getElementById('enviar'); 
+const mensaje = document.getElementById('mensaje'); 
+const mail = document.getElementById('mail'); 
+const divMensajes = document.getElementById('mensajes')
+const fecha = new Date(); 
+
+
+form.addEventListener('submit',(e) =>{
+    e.preventDefault(); 
+    
+    const product = {
+        nombre: nombre.value,
+        precio: precio.value,
+        url: url.value
+    }; 
+    socket.emit('newProduct',product); 
+})
+
+socket.on('productos',(products) =>{
+    productos.innerHTML = products.map(prod =>{
+        return(
+            `<div class="card">
+                <h5 class="card-title">Lista de productos</h5>
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nombre</th>
+                                <th scope="col">Precio </th>
+                                <th scope="col">Url</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>${prod.nombre}</td>
+                                <td>${prod.precio}</td>
+                                <td>${prod.url}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>   
+            </div>
+            `
+        )
+    })
+})
+
+
+enviar.addEventListener('click',() =>{
+    const menssage = {
+        texto: mensaje.value,
+        mail: mail.value
+    }
+
+    if(mail.value == ''){
+        alert('Debe ingresar su mail!')
+    }else{
+        socket.emit('newMessage',menssage); 
+        mensaje.value = ''; 
+        console.log(menssage);
+    }
+    
+})
+
+socket.on('messages',(messages) =>{
+    divMensajes.innerHTML = messages.map(message =>{
+        return(
+            `<div>
+                <strong>${message.mail} [${fecha.getDate()}/${fecha.getMonth()}/${fecha.getFullYear()}]: </strong>
+                <em>${message.texto}</em>
+            </div>`
+        )
+    })
+})
 
 
 
